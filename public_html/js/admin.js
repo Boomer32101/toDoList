8aeff5d6a0f46429d0413b713489b2757abbf73d
Owen Vandevelde <owen.vande@gmail.com>
2016-04-28 09:02:34 -0700

    loggin finished

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -27,8 +27,14 @@ function Posts(args){
     this.content = args.content || "";
     this.authorEmail = args.authorEmail || "";
 }
-function userLoggedIn(){
+function userLoggedIn(user){
     console.log("you logged in");
+    
+    var welcomeScript = $('#welcome-template').html();
+    var welcomeTemplate = Handlebars.compile(welcomeScript)
+    var welcomeHTML = welcomeTemplate(user);
+    
+    $('.main-container').html(welcomeHTML)
 }
 function gotError(){
     console.log("error message - " + error.message)
